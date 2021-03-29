61b6bf366394b34f15a02df370baf4fa4ea301ec
shreyanshi <git>
2021-03-29 11:17:47 +0530

    i'm uploading

---
+++ src/ArryList.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class ArryList {
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<Integer>();
+        list.add(1000);
+        list.add(200);
+        list.add(3000);
+        list.add(4999);
+        list.add(200);
+        list.add(3000);
+        list.add(null);
+        list.add(null);
+        for (int i=0; i<=list.size(); i++){
+            System.out.println(list);
+    }
+    }
+}
